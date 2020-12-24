b4e27a16fa331ed5b669f869eac00b2b08701315
Justinas Grigas <jstn_as@protonmail.com>
2020-12-24 06:05:54 +0000

    Changed font, moved colours to seperate file.

--- config.def.h
+++ config.def.h
@@ -4,14 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Gohu GohuFont:style=Regular:size=8"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+#include "palette.c"
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = {col_gray3, col_gray1},
+	[SchemeSel] = {col_gray4, col_cyan},
+	[SchemeOut] = {col_gray1, col_gray3},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
